795a990c5bfbd428d34c0fcb686edbf1011c1dec
Sacha Baranes <sbaranes@z3r5p5.42lyon.fr>
2022-01-04 12:34:07 +0100

    add at / push_back / realloc / destroy_tab

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -10,31 +10,103 @@ namespace ft
 	template < typename T, typename Allocator = std::allocator<T> >
 	class vector{
 
+	private:
+		T			*my_tab;
+		size_t		size_max_allocated;
+		size_t		size_max_construct;
+		Allocator	alloc;
 	public:
-		T *my_tab;
-		Allocator alloc;
 
-		vector(size_t N, Allocator alloc = Allocator()) : alloc(alloc)
+		class OutOfLimitsAlocatedException: public std::exception
+		{
+			virtual const char* what() const throw()
+			{
+				return ("Occurence not allocated");
+			}
+		};
+		vector(Allocator alloc = Allocator()) : size_max_allocated(0), size_max_construct(0), alloc(alloc)
+		{
+			my_tab = NULL;
+		};
+
+		vector(size_t N, Allocator alloc = Allocator()) : size_max_allocated(N), size_max_construct(N), alloc(alloc)
 		{
 			my_tab = alloc.allocate(N);
 			for (size_t i = 0; i < N; i++)
 			{
 				alloc.construct(&my_tab[i], T());
 			}
-
 		};
 
-		int at (size_t index)
+		vector(size_t N, T element, Allocator alloc = Allocator()) : size_max_allocated(N), size_max_construct(N), alloc(alloc)
 		{
-			return this->my_tab[index];
+			my_tab = alloc.allocate(N);
+			for (size_t i = 0; i < N; i++)
+			{
+				alloc.construct(&my_tab[i], element);
+			}
+		};
+
+		size_t	capacity() { return (size_max_allocated); }
+		size_t	size() { return (size_max_construct); }
+
+		size_t at (size_t index)
+		{
+			if (size() < index)
+				throw OutOfLimitsAlocatedException();
+			else
+				return (this->my_tab[index]);
 		}
-		~vector()
+
+		void destroy_tab()
 		{
-			for (size_t i = 0; i < count; i++) // taille de tout ceux qui est construct.
+			for (size_t i = 0; i < size(); i++)
 			{
 				alloc.destroy(&my_tab[i]);
 			}
-			alloc.deallocate(my_tab, size_max) // taille de tout ceux qui est allocate.
+			alloc.deallocate(my_tab, capacity());
+			my_tab = NULL;
+		}
+
+		void	my_realloc_size()
+		{
+			T *temp = NULL;
+			if (capacity() == 0)
+			{
+				temp = alloc.allocate(1);
+				size_max_allocated = 1;
+			}
+			else
+			{
+				temp = alloc.allocate((capacity() * 2));
+				size_max_allocated = capacity() * 2;
+			}
+
+			for (size_t i = 0; i < size(); i++)
+			{
+				alloc.construct(&temp[i], my_tab[i]);
+			}
+			destroy_tab();
+			my_tab = temp;
+		}
+
+		void push_back(T new_insert)
+		{
+			if (capacity() > size())
+			{
+				alloc.construct(&my_tab[size()], new_insert);
+				size_max_construct++;
+			}
+			else
+			{
+				my_realloc_size();
+				alloc.construct(&my_tab[size()], new_insert);
+				size_max_construct++;
+			}
+		}
+		~vector()
+		{
+			destroy_tab();
 		}
 	};
 }
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,11 +3,22 @@
 int main()
 {
 	std::cout << "------- Start Ft_Container -------" << std::endl;
-	ft::vector<int, std::allocator<int> > test(5);
+	ft::vector<int, std::allocator<int> > my_vector;
+	std::vector<int, std::allocator<int> > realvector;
 
-	for (size_t i = 0; i < 5; ++i)
+
+	my_vector.push_back(1);
+	my_vector.push_back(2);
+	my_vector.push_back(3);
+	my_vector.push_back(4);
+	my_vector.push_back(5);
+	for (size_t i = 0; i < my_vector.size(); ++i)
 	{
-		std::cout << test.at(i) << ";\n";
+		std::cout << "index num = " << i << " = " << my_vector.at(i) << ";\n";
 	}
+	std::cout << "capacity = " << my_vector.capacity() << std::endl;
+	std::cout << "size = " << my_vector.size() << std::endl;
+	// std::cout << "max_size = " << my_vector.max_size() << std::endl;
 	std::cout << "end of vector" << std::endl;
+
 }
